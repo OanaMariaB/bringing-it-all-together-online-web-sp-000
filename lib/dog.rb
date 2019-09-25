a18c2025495361597cd994981c81b76d83eb860c
Oana Berentan <oana.berentan@yahoo.com>
2019-09-25 19:10:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,10 +77,12 @@ class Dog
   end
 
   def update
-    sql =<<-SQL
-     "UPDATE dogs SET name = ? AND breed = ? WHERE id = ?"
-     SQL
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
+    sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
+
 
 end #class
