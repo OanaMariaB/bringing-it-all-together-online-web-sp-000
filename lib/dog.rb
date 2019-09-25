60b47dfcb4424803e99237309c78dcd4c4e4581f
Oana Berentan <oana.berentan@yahoo.com>
2019-09-25 18:38:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,63 @@
+class Dog
+
+  attr_accessor :id, :name, :breed
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql =<<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql =<<-SQL
+    DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql =<<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+  def self.create(hash_of_attributes)
+    dog = self.new(hash_of_attributes)
+    dog.save
+    dog
+  end
+
+  def self.new_from_db(row)
+    new_dog = self.new(name:row[1], breed:row[2])
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
+    new_dog
+  end
+
+  def self.find_by_id(id)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id).flatten
+    new_dog = self.new(id:dog[0], name:dog[1], breed:dog[2])
+  end
+
+  def self.find_or_create_by
+  end
+
+
+end #class
